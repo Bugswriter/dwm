a31c7118ef8a13c7b0e558ea129ec1f36853884c
Suraj Kushwah <suraj@bugswriter.com>
2021-10-23 00:52:33 +0530

    adding new font awesome

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
